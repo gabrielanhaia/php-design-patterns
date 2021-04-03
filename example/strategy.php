75cb43fccedad9f325e7c06b2857043e75f5d056
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2021-04-03 11:10:07 +0200

    (Strategy): Create strategy pattern example

--- example/strategy.php
+++ example/strategy.php
@@ -1,5 +1,24 @@
 <?php
 
+use App\Strategy\AnimalTaxStrategy;
+use App\Strategy\Component\Product;
+use App\Strategy\Context;
+use App\Strategy\TaxFreeStrategy;
+use App\Strategy\VegetablesTaxStrategy;
+
 require_once 'autoloader.php';
 
 
+$product = new Product;
+$product->setName('Product Test')
+    ->setType('vegetable')
+    ->setPrice(100);
+
+$strategy = new TaxFreeStrategy;
+//$strategy = new VegetablesTaxStrategy;
+//$strategy = new AnimalTaxStrategy;
+$context = new Context($strategy);
+
+$context->calculateProduct($product);
+
+echo $product->getTaxes();
