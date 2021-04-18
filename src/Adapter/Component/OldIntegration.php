68ca8d1a5fe1c87ea12f50f13bfe70c3d7340c1d
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2021-04-18 18:06:03 +0200

    (Adapter) Change the user names in the Adapter implementation

--- src/Adapter/Component/OldIntegration.php
+++ src/Adapter/Component/OldIntegration.php
@@ -14,11 +14,15 @@ class OldIntegration
         return [
             [
                 'uuid' => '7de2a62e-7628-4d2f-a02b-fe395c5ce85b',
-                'name' => 'Red Wine',
+                'name' => 'Gabriel Anhaia',
+            ],
+            [
+                'uuid' => '7de2a62e-7628-4d2f-a02b-fe395c5ce85b',
+                'name' => 'Brian Park',
             ],
             [
                 'uuid' => '41e72c66-1998-4ea5-9ce8-12eef91e267d',
-                'name' => 'Iphone 12 Pro Max'
+                'name' => 'Roderick Mcbride'
             ]
         ];
     }
