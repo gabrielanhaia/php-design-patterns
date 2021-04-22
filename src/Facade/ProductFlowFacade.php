3faf1eac959c637177059b1c8d42fa5da398a3e0
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2021-04-22 21:11:38 +0200

    Implement Facade pattern

---
+++ src/Facade/ProductFlowFacade.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Facade;
+
+use App\Facade\Component\BarCodeUtil;
+use App\Facade\Component\IntegrationSoapErp;
+use App\Facade\Component\MarketplaceIntegration;
+use App\Facade\Component\Product;
+use App\Facade\Component\ProductRepository;
+
+/**
+ * Class ProductFlowFacade
+ */
+class ProductFlowFacade
+{
+    private ProductRepository $productRepository;
+
+    private MarketplaceIntegration $marketplaceIntegration;
+    private IntegrationSoapErp $integrationSoapErp;
+
+    public function __construct(
+        ProductRepository $productRepository,
+        MarketplaceIntegration $marketplaceIntegration,
+        IntegrationSoapErp $integrationSoapErp
+    )
+    {
+        $this->productRepository = $productRepository;
+        $this->marketplaceIntegration = $marketplaceIntegration;
+        $this->integrationSoapErp = $integrationSoapErp;
+    }
+
+    public function createProduct(string $name, float $initialPrice, int $initialStock = 0): void
+    {
+        $product = (new Product)
+            ->setBarCode(BarCodeUtil::generateBarCode())
+            ->setName($name)
+            ->setPrice($initialPrice)
+            ->setStock($initialStock)
+            ->setUuid('3786ebae-4816-46a8-88ac-7aa5b530524f');
+
+        $this->productRepository->persist($product);
+
+        $this->marketplaceIntegration->createProduct($product);
+
+        $this->marketplaceIntegration->insertPrice($product->getUuid(), $product->getPrice());
+
+        $this->marketplaceIntegration->insertStock($product->getUuid(), $product->getStock());
+
+        $product->setIsOnMarketplace(true);
+
+        $this->productRepository->update($product);
+
+        $this->integrationSoapErp->setProductOnMarketplace($product, true);
+    }
+}
